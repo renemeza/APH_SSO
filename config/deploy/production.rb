1498de2581af924267b958a49901db04d7b4784a
Rene Meza Vasquez <rene.meza125@gmail.com>
2015-08-31 14:59:49 -0600

    Deploy fixes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -17,7 +17,7 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
+role :app, %w{deploy@dev.aphtech.org}
 # role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
 # role :db,  %w{deploy@example.com}
 
