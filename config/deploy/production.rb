81f0c40bd8607d9b782c45dfcd48c8771fb28375
Rene Meza Vasquez <rene.meza125@gmail.com>
2015-08-31 16:24:50 -0600

    d

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'dev.aphtech.org', user: 'deploy', roles: %w{app db web}
+server 'dev.aphtech.org', user: 'deploy', roles: %w{app web}
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
@@ -19,7 +19,7 @@ server 'dev.aphtech.org', user: 'deploy', roles: %w{app db web}
 
 role :app, %w{deploy@dev.aphtech.org}
 role :web, %w{deploy@dev.aphtech.org}
-role :db,  %w{deploy@dev.aphtech.org}
+# role :db,  %w{deploy@dev.aphtech.org}
 
 
 
